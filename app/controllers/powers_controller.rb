f56cd6e2f3d0acd0b05164f3098cb5dd4aa3faa4
github username <github email address>
2019-05-24 11:16:32 -0700

    Basic Functionality Done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,35 @@
 class PowersController < ApplicationController
+  before_action :find_power, only: [:show, :edit, :update]
   def index
     @powers = Power.all
+  end
+
+  def show
+  end
+
+  def new
+  	@power = Power.new
+  end
+
+  def create
+  	@power = Power.create(power_params)
+  end
+
+  def edit
+  end
+
+  def update
+  	@power = Power.update(power_params)
+  end
+
+
+  private
+
+  def find_power
+  	@power = Power.find(params[:id])
+  end
+
+  def power_params
+  	params.require(:power).permit(:name, :description)
   end
 end
