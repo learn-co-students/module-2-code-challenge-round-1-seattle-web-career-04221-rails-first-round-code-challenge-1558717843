59d8c5751b9e15b182c8306c82366f3ca24d66c6
Hanna Engel <hengel@alumni.berklee.edu>
2019-05-24 11:20:52 -0700

    nailed it

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+      if @heroine.valid?
+        @heroine.save
+        redirect_to @heroine
+      else
+        flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+private
+
+def heroine_params
+  params.require(:heroine).permit!
+end
+
 end
